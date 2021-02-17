3c01a45a62269a94627d8f572ff2b915706af60f
bzhong88 <berniezhong@gmail.com>
2021-02-16 22:33:00 -0500

    update

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,1 +1,36 @@
 /* Put your javascript in here */
+let width = 130;
+let count = 3;
+
+let list = carousel.querySelector('ul');
+let listElems = carousel.querySelectorAll('li');
+
+let position = 0; // ribbon scroll position
+
+carousel.querySelector('.prev').onclick = function() {
+  // shift left
+  position += width * count;
+  // can't move to the left too much, end of images
+  position = Math.min(position, 0)
+  list.style.marginLeft = position + 'px';
+};
+
+carousel.querySelector('.next').onclick = function() {
+  // shift right
+  position -= width * count;
+  // can only shift the ribbbon for (total ribbon length - visible count) images
+  position = Math.max(position, -width * (listElems.length - count));
+  list.style.marginLeft = position + 'px';
+};
+
+function arraysushi() {
+  const iarray = []; 
+  const images = [document.querySelector('.image')];
+  images.forEach((element) => {
+    const image = element.querySelector('li');
+    console.log(element);
+    iarray.append(image);
+  })
+  console.log(iarray);
+}
+arraysushi()
